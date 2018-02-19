0ce37ea6d822197a5ef905d19156de48e782221e
sancho <developersancho@gmail.com>
2018-02-19 11:43:43 +0300

    Initial Commit

---
+++ SwiftJsonParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftJsonParse
+//
+//  Created by developersancho on 19.02.2018.
+//  Copyright © 2018 developersancho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
